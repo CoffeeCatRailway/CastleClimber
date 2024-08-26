0a9d534f0195f95b9f74ed45bfc90a9f34f6dd64
CoffeeCatRailway <dragonlee1002@gmail.com>
2024-08-26 21:36:29 +1000

    Random level generator

--- scripts/levels/FlooredLevel.gd
+++ scripts/levels/FlooredLevel.gd
@@ -4,20 +4,26 @@ const FLAG_PASSAGE_LEFT: int = 0b100
 const FLAG_PASSAGE_MIDDLE: int = 0b010
 const FLAG_PASSAGE_RIGHT: int = 0b001
 
-var floorsEnd: Dictionary = {
-	FLAG_PASSAGE_RIGHT: "res://scenes/levels/floors/end/floor_end_right.tscn"
+const FLOORS_END: Dictionary = {
+	FLAG_PASSAGE_RIGHT: "res://scenes/levels/floors/end/floor_end_right.tscn",
+	FLAG_PASSAGE_LEFT: "res://scenes/levels/floors/end/floor_end_left.tscn",
 }
-var floorsMid: Dictionary = {
-	getMiddleFloorKey(FLAG_PASSAGE_RIGHT, FLAG_PASSAGE_RIGHT): "res://scenes/levels/floors/mid/floor_mid_right_right.tscn"
+var FLOORS_MID: Dictionary = {
+	getMiddleFloorKey(FLAG_PASSAGE_RIGHT, FLAG_PASSAGE_RIGHT): "res://scenes/levels/floors/mid/floor_mid_right_right.tscn",
+	getMiddleFloorKey(FLAG_PASSAGE_LEFT, FLAG_PASSAGE_RIGHT): "res://scenes/levels/floors/mid/floor_mid_left_right.tscn",
+	getMiddleFloorKey(FLAG_PASSAGE_RIGHT, FLAG_PASSAGE_LEFT): "res://scenes/levels/floors/mid/floor_mid_right_left.tscn",
+	getMiddleFloorKey(FLAG_PASSAGE_LEFT, FLAG_PASSAGE_LEFT): "res://scenes/levels/floors/mid/floor_mid_left_left.tscn",
+	getMiddleFloorKey(FLAG_PASSAGE_LEFT | FLAG_PASSAGE_RIGHT, FLAG_PASSAGE_LEFT | FLAG_PASSAGE_RIGHT): "res://scenes/levels/floors/mid/floor_mid_left_right_left_right.tscn",
 }
-var floorsStart: Dictionary = {
-	FLAG_PASSAGE_RIGHT: "res://scenes/levels/floors/start/floor_start_right.tscn"
+const FLOORS_START: Dictionary = {
+	FLAG_PASSAGE_RIGHT: "res://scenes/levels/floors/start/floor_start_right.tscn",
+	FLAG_PASSAGE_LEFT | FLAG_PASSAGE_RIGHT: "res://scenes/levels/floors/start/floor_start_left_right.tscn",
 }
 
 @onready var levelDoorHandler: LevelDoorHandler = $LevelDoorHandler
 @onready var tilemapMain: TileMap = $TileMap
 
-@export_range(2, 100, 1, "or_greater") var floors: int = 2
+@export_range(2, 100, 1, "or_greater") var floorCount: int = 2
 
 var doorEntrance: Door
 var doorExit: Door
@@ -25,20 +31,142 @@ var doorExit: Door
 func _ready() -> void:
 	call_deferred("generateCastle")
 
+func padBinary(decimal: int, pad: int) -> String:
+	return String.num_int64(decimal, 2).pad_zeros(pad)
+
 func generateCastle() -> void:
 	var time: int = Time.get_ticks_usec()
 	#await get_tree().create_timer(1.).timeout
 	
+	# Chose floor keys
+	#var endKey: int = FLOORS_END.keys().pick_random()
+	#print("Chose key '", padBinary(endKey, 3), "' for top floor")
+	#
+	#var middleKeys: Array[int] = []
+	#if floorCount > 2:
+		#middleKeys.resize(floorCount - 2)
+		#var lastKey: int = endKey
+		#var i: int = floorCount - 2
+		#
+		#var trys: int = 0
+		#var maxTrys: int = 10
+		#
+		#var skipKey: int = -1
+		#while i > 0:# && trys < maxTrys:
+			#var floors: Array = FLOORS_MID.keys()
+			#if skipKey != -1:
+				#floors.erase(skipKey)
+				#print("Skipping key '", padBinary(skipKey, 6), "'")
+				#skipKey = -1
+			#
+			#var key: int = floors.pick_random()
+			#var top: int = key & 0b000111
+			#
+			##print("f", (i + 1), "-", padBinary(lastKey, 3), " & f", i, "-", padBinary(top, 3), " = ", padBinary(lastKey & top, 3))
+			#if lastKey & top != 0:
+				#middleKeys[i - 1] = key
+				##middleKeys.append(key)
+				#print("Chose key '", padBinary(key, 6), "' for f", i)
+				#i -= 1
+				#lastKey = key >> 3 # Last key is 'bottom passages' of floor
+				#trys = 0
+			#else:
+				#trys += 1
+			#
+			#if trys >= maxTrys: ## Failed because 'lastKey' before 'i+1' is unknown
+				#i = clampi(i + 1, 0, middleKeys.size() - 1)
+				#lastKey = middleKeys[clampi(i + 1, 0, middleKeys.size() - 1)] >> 3
+				#skipKey = key
+				#print("Retrying f", i, " after not finding possible key (", padBinary(key, 6), ") for f", (i - 1), " after ", trys, " trys...")
+				#trys = 0
+		##if trys >= maxTrys:
+			##printerr("Coundn't find floor to place after `", padBinary(middleKeys[-1], 6), "' in ", trys, " trys!")
+	#
+	#var startKey: int = FLAG_PASSAGE_RIGHT#FLOORS_START.keys().pick_random()
+	#
+	#for key: int in middleKeys:
+		#print(padBinary(key, 6))
+	
+	# Method: Filter keys by passages
+	var endKey: int = FLOORS_END.keys().pick_random()
+	print("Chose key '", padBinary(endKey, 3), "' for f", (floorCount - 1))
+	
+	var middleKeys: Array[int] = []
+	middleKeys.resize(floorCount - 2)
+	var startKey: int
+	
+	var i: int = floorCount - 2 # Start at second from top
+	var lastKey: int = endKey << 3
+	
+	var skipKey: int = -1
+	
+	while i >= 0:
+		var isBottomFloor: bool = i == 0
+		var lastKeyBottom: int = lastKey >> 3
+		
+		print("\nFiltering possible floor (f", i, ") for last '", padBinary(lastKeyBottom, 3), "'")
+		var possibleKeys: Array = (FLOORS_START if isBottomFloor else FLOORS_MID).keys().filter(func(_key: int) -> bool: return (_key & 0b000111) & lastKeyBottom != 0)
+		#print(possibleKeys.map(func(a): return padBinary(a, 6)))
+		
+		if skipKey != -1:
+			#print(possibleKeys.map(func(_key: int) -> String: return padBinary(_key, 6)))
+			possibleKeys.erase(skipKey)
+			print("Skipping key '", padBinary(skipKey, 6), "'")
+			if possibleKeys.size() == 0:
+				printerr("0 possible keys!")
+				get_tree().quit(-1)
+				return
+			skipKey = -1
+		
+		if possibleKeys.size() == 0:
+			printerr("No possible floor for f", i, " last key: ", padBinary(lastKey, 6))
+			#print(middleKeys.map(func(_key: int) -> String: return padBinary(_key, 6)))
+			#return
+			
+			i = clampi(i + 1, 0, floorCount - 2) # Move up floor
+			skipKey = lastKey
+			var lastI: int = clampi(i + 1, 0, floorCount - 2) # Get key above floor i
+			print("Retrying f", i, " & skipping last key. Index ", lastI)
+			lastKey = (endKey << 3) if lastI >= middleKeys.size() else middleKeys[lastI]
+			continue
+			
+			#print("Retrying f", i, " after not finding possible key (", padBinary(key, 6), ") for f", (i - 1), " after ", trys, " trys...")
+			#skipKey = lastKey
+			#i = clampi(i + 1, 0, floorCount - 2) # Move up floor
+			#print("Retrying f", i, ". No possible floor for f", (i - 1), " key: ", padBinary(skipKey, 6))
+			#lastKey = middleKeys[clampi(i + 1, 0, floorCount - 2)] # Get key above floor i
+			#continue
+		
+		var key: int = possibleKeys.pick_random()
+		if isBottomFloor:
+			startKey = key
+		else:
+			#middleKeys.append(key)
+			middleKeys[i - 1] = key
+		print("Chose key '", padBinary(key, (3 if isBottomFloor else 6)), "' for f", i)
+		i -= 1
+		lastKey = key# >> (0 if isBottomFloor else 3)
+	
+	# Place floors
 	var y: int = 0
-	y = placeFloor(floorsEnd, FLAG_PASSAGE_RIGHT, y)
-	y = placeFloor(floorsMid, getMiddleFloorKey(FLAG_PASSAGE_RIGHT, FLAG_PASSAGE_RIGHT), y)
-	#y = placeFloor(floorsMid, getMiddleFloorKey(FLAG_PASSAGE_RIGHT, FLAG_PASSAGE_RIGHT), y)
-	y = placeFloor(floorsStart, FLAG_PASSAGE_RIGHT, y)
+	#y = placeFloor(FLOORS_END, FLAG_PASSAGE_RIGHT, y)
+	#y = placeFloor(FLOORS_MID, getMiddleFloorKey(FLAG_PASSAGE_RIGHT, FLAG_PASSAGE_RIGHT), y)
+	#y = placeFloor(FLOORS_START, FLAG_PASSAGE_RIGHT, y)
+	#y = placeFloor(FLOORS_MID, getMiddleFloorKey(FLAG_PASSAGE_LEFT, FLAG_PASSAGE_RIGHT), y)
+	#y = placeFloor(FLOORS_START, FLAG_PASSAGE_LEFT | FLAG_PASSAGE_RIGHT, y)
+	
+	y = placeFloor(FLOORS_END, endKey, y)
+	if floorCount > 2:
+		for j: int in range(floorCount - 3, -1, -1): # Iterate backwards
+			y = placeFloor(FLOORS_MID, middleKeys[j], y)
+		#for key: int in middleKeys:
+			#y = placeFloor(FLOORS_MID, key, y)
+	y = placeFloor(FLOORS_START, startKey, y)
 	
 	print("Castle generation took %s milliseconds" % [float(Time.get_ticks_usec() - time) / 1000.])
 	
 	$Player.global_position = doorEntrance.marker.global_position
-	$Player/Camera2D.updateLimits()
+	$Camera2D.updateLimits()
 	
 	levelDoorHandler.doorEntrance = doorEntrance
 	levelDoorHandler.doorExit = doorExit
